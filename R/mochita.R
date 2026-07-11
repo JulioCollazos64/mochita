7e98e2e646240f415715c56bc929cb211e9a74a1
Julio Collazos <amarullazo626@gmail.com>
2026-07-11 15:32:42 +1000

    added expect method
    allow to test status codes and response headers

--- R/mochita.R
+++ R/mochita.R
@@ -1,5 +1,5 @@
-mochita <- function() {
-  Mochita$new()
+mochita <- function(app) {
+  Mochita$new(app)
 }
 
 Mochita <- R6::R6Class(
@@ -13,12 +13,12 @@ Mochita <- R6::R6Class(
             stopifnot(is.character(path))
 
             private$path <- path
-            private$method <- method
+            private$method <- METHOD
 
             invisible(self)
           },
           env = list(
-            method = quote(method)
+            METHOD = method
           )
         )
 
@@ -29,6 +29,39 @@ Mochita <- R6::R6Class(
       private$headers[[name]] <- value
       invisible(self)
     },
+    expect = function(...) {
+      args <- list(...)
+
+      stopifnot(length(args) <= 2)
+
+      if (length(args) == 1) {
+        stopifnot(is.numeric(..1))
+        status <- ..1
+        test <- function(res) {
+          testthat::expect_equal(res$status, status)
+        }
+        private$tests <- append(private$tests, test)
+        return(invisible(self))
+      }
+
+      name <- ..1
+      value <- ..2
+
+      stopifnot(is.character(name), is.character(value))
+
+      private$responseHeaders <- c(private$responseHeaders, name)
+
+      test <- function(res) {
+        testthat::expect_identical(
+          res$headers[[name]],
+          value
+        )
+      }
+
+      private$tests <- append(private$tests, test)
+
+      invisible(self)
+    },
     getPrivate = function() {
       private
     }
@@ -36,7 +69,9 @@ Mochita <- R6::R6Class(
   private = list(
     path = character(0),
     method = character(0),
-    headers = list()
+    headers = list(),
+    tests = list(),
+    responseHeaders = character(0)
   ),
   lock_objects = FALSE,
   cloneable = FALSE
